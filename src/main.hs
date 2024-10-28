6b257bd89b43555e2b27b3e6bff0e0f85ced06ef
CrisCLo <95257381+CrisCLo@users.noreply.github.com>
2024-10-28 17:21:02 -0400

    Completed tutorial

--- src/main.hs
+++ src/main.hs
@@ -1,11 +1,34 @@
 -- Write printAMessage here
+printAMessage :: Show a => a -> IO ()
+printAMessage x = print x
 
 -- Write division here
+division :: Double -> Double -> Maybe Double
+division _ 0 = Nothing
+division x y = Just (x / y)
 
 -- Write factorial here
+factorial :: Int -> Int
+factorial 0 = 1
+factorial n = product [1..n]
 
 -- Write factList here
+factList :: Int -> [Int]
+factList n = [factorial i | i <- [1..n]]
 
 -- Write merge here
+merge :: [Int] -> [Int] -> [Int]
+merge [] ys = ys
+merge xs [] = xs
+merge (x:xs) (y:ys)
+    | x <= y    = x : merge xs (y:ys)
+    | otherwise = y : merge (x:xs) ys
 
-main = print "Test" -- Replace this with your testing code
+main :: IO ()
+main = do
+    printAMessage "Hello, Haskell!"
+    print ("division 10 2:", division 10 2)
+    print ("division 10 0:", division 10 0)
+    print ("factorial 5:", factorial 5)
+    print ("factList 5:", factList 5)
+    print ("merge [1,3,5] [2,4,6]:", merge [1,3,5] [2,4,6]) -- Replace this with your testing code
